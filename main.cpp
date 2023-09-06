31a49e7fa953b69051b97cca1c27d856229cdc2e
Dickson Pinheiro <dicksonpinheiro4@gmail.com>
2023-09-05 21:34:35 -0300

    feat: compare binary search and sequenial search in c++

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <ctime>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+int binary_search(int nums[], int item, int begin, int end){
+    int center = (begin + end)/2;
+    if(nums[center] == item){
+        return center;
+    }
+    if(begin > end){
+        return -1;
+    }
+    if(nums[center] < item){
+        return binary_search(nums, item, center + 1, end);
+    } else {
+        return binary_search(nums, item, begin, center - 1);
+    }
+}
+
+
+int sequencial_search(int nums[], int item, int size){
+    for(int i = 0; i < size; i++){
+        if(nums[i] == item){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+int main()
+{
+    int vet[1000];
+    vet[0] = 1;
+    clock_t time_req;
+    for(int i = 10; i <= 1000; i++){
+        for(int j = 0; j < i; j++){
+            vet[j] = i*j;
+        }
+        time_req = clock();
+        for(int k = 0; k < 100; k++){
+            /*
+            binary_search(vet, 23, 0, i);
+            */
+           sequencial_search(vet, 23, i);
+        }
+        time_req = clock()- time_req;
+        cout << i << " " << (float)time_req/CLOCKS_PER_SEC << endl;
+    }
+    /*
+    int result_binary = binary_search(vet, 6, 0, size);
+    int result_linear = linear_search(vet, 5, size);
+    cout << result_linear << endl;
+    cout << result_binary << endl;
+    */
+    
+    return 0;
+}
+
+
+
