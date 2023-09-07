aaa355e97e66098a4ffa57372b5243abfc74d9c1
Dickson Pinheiro <dicksonpinheiro4@gmail.com>
2023-09-07 02:00:06 -0300

    feat: update params

--- main.cpp
+++ main.cpp
@@ -34,44 +34,34 @@ int sequencial_search(int nums[], int item, int size){
 int main()
 {
     std::srand(std::time(nullptr));
-    int vet[1000];
-    vet[0] = 1;
+    int vet[200000];
     clock_t start;
     clock_t end;
     float soma_total;
-    clock_t time_req;
     float tempo;
-    for(int i = 10; i <= 1000; i+=10){
+    for(int i = 100; i <= 200000; i+=100){
         for(int j = 0; j < i; j++){
             vet[j] = i*j;
         }
         
         soma_total = 0.0;
-        
         for(int k = 0; k < 1000; k++){
             int random = std::rand() % i*2;
+            
             start = clock();
             binary_search(vet, random, 0, i);
             end = clock();
-            
             /*
+            start = clock();
             sequencial_search(vet, random, i);
+            end = clock();
             */
-            
-           
+
             tempo = static_cast<double>(end - start) / CLOCKS_PER_SEC;
            
             soma_total += tempo;
         }
-        
-        cout << i << " " << soma_total / 1000 << endl;
+        cout << i << " " << soma_total/1000 << endl;
     }
-    /*
-    int result_binary = binary_search(vet, 6, 0, size);
-    int result_linear = linear_search(vet, 5, size);
-    cout << result_linear << endl;
-    cout << result_binary << endl;
-    */
-    
     return 0;
 }
